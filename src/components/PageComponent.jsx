9df1b68d1dd86fb741ba40687b510760a93006b6
Jaydee-31 <johndextergamo@gmail.com>
2024-03-25 13:23:26 +0800

    Added survey form

--- src/components/PageComponent.jsx
+++ src/components/PageComponent.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function PageComponent({ title, buttons = [], children }) {
 	return (
 		<>
--- src/router.jsx
+++ src/router.jsx
@@ -6,6 +6,7 @@ import DefaultLayout from "./components/DefaultLayout";
 import GuestLayout from "./components/GuestLayout";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
+import SurveyView from "./views/SurveyView";
 
 const router = createBrowserRouter([
 	{
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
 			{
 				path: "/surveys",
 				element: <Surveys />,
+			},
+			{
+				path: "/surveys/create",
+				element: <SurveyView />,
 			},
 		],
 	},
